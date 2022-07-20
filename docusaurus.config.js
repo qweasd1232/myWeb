3e747f654cb9436215bf3606624b0265cc45424f
zhouze.jiang <1032274155@qq.com>
2022-07-20 14:14:03 +0800

    添加了部署指导文件，和对应图片

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,16 +8,16 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://qweasd1232.github.io/myWeb/',
-  baseUrl: '/',
+  url: 'https://qweasd1232.github.io',
+  baseUrl: '/myWeb/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'qweasd1232', // Usually your GitHub org/user name.
+  projectName: 'myWeb', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
